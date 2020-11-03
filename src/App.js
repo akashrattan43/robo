aec8ba7de3ee6ed1cf2bbf53613024e3b5fcfc9f
akash rattan <akashrattan43@Gmail.com>
2020-11-03 15:58:39 -0800

    mount

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react'
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      string: 'Hello Akash'
+      monsters: []
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.string}
-        </p>
-          <button onClick={ () => this.setState({string: 'Hello Andrei'})}>
-            Change text
-        </button>
-        </header>
+        {
+          this.state.monsters.map(monster => <h1> {monster.name} </h1>)
+        }
       </div>
     )
   }
